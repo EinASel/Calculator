1f9d1acc98bf7347efd03351d8680b4f364fc0aa
esel6 <esel6@DESKTOP-NMP8GTB>
2018-09-21 17:23:12 -0700

    This is my calculator project using c++

---
+++ Calculator/src/Calculator.cpp
@@ -0,0 +1,65 @@
+//============================================================================
+// Name        : Calculator.cpp
+// Author      : Ein Sel
+// Version     : V.1
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int add (int, int);
+int sub (int, int);
+int mult (int, int);
+int divi (int, int);
+
+int main() {
+	int x;
+	int x2;
+	char op;
+	cout << "Enter your first number" << endl;
+	cin >> x;
+	cout << "Now enter your second number" << endl;
+	cin >> x2;
+	cout << "Now enter your operation (+, -, *, /)" << endl;
+	cin >> op;
+
+	if (op == '+'){
+		cout << add(x, x2);
+	}
+
+	else if (op == '-'){
+		cout << sub (x, x2);
+	}
+
+	else if (op == '*'){
+		cout << mult (x, x2);
+	}
+
+	else if (op == '/'){
+		cout << divi (x, x2);
+	}
+
+	else {
+		cout << "Please enter correct operation" << endl;
+	}
+
+	return 0;
+}
+
+int add (int a, int b){
+	return a+b;
+}
+
+int sub (int a, int b){
+	return a-b;
+}
+
+int mult (int a, int b){
+	return a*b;
+}
+
+int divi (int a, int b){
+	return a/b;
+}
